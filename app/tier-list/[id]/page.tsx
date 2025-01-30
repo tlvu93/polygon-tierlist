c4e7eeb4e612436e89af357b0b55a5dbf3941423
Tuan <tv@codestryke.com>
2025-01-30 17:39:13 +0100

    Fix fix async warning for Dynamic API

--- app/tier-list/[id]/page.tsx
+++ app/tier-list/[id]/page.tsx
@@ -1,6 +1,7 @@
 import Dashboard from "@/components/Dashboard";
 
-export default function TierListPage({ params }: { params: { id: string } }) {
+export default async function TierListPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   // You can use the id to fetch the specific tier list data
   console.log("Tier List ID:", params.id);
 
