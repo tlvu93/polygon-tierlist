9b0ccbdf1469df873ca2a6eff0a71fad02f2db47
Tuan Linh Vu <tuan.vu@prothinx.io>
2025-07-04 21:14:23 +0200

    Undo the deed

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -5,9 +5,9 @@ import { redirect } from "next/navigation";
 export const dynamic = "force-dynamic";
 
 type Props = {
-  searchParams: {
+  searchParams: Promise<{
     message?: string;
-  };
+  }>;
 };
 
 export default async function LoginPage({ searchParams }: Props) {
@@ -16,7 +16,7 @@ export default async function LoginPage({ searchParams }: Props) {
     data: { session },
   } = await supabase.auth.getSession();
 
-  const { message } = searchParams;
+  const { message } = await searchParams;
 
   if (session) {
     redirect("/dashboard");
@@ -24,7 +24,11 @@ export default async function LoginPage({ searchParams }: Props) {
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-gray-50">
-      {message && <div className="mb-4 p-4 bg-blue-100 text-blue-700 rounded-md">{message}</div>}
+      {message && (
+        <div className="mb-4 p-4 bg-blue-100 text-blue-700 rounded-md">
+          {message}
+        </div>
+      )}
       <LoginForm />
     </div>
   );
--- app/tier-list/[id]/page.tsx
+++ app/tier-list/[id]/page.tsx
@@ -1,10 +1,19 @@
 import TierListLayout from "@/components/tier-list/TierListLayout";
 import { createClient } from "@/utils/supabase/server";
 
-export default async function TierListPage({ params }: { params: { id: string } }) {
+export default async function TierListPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const supabase = await createClient();
+  const { id } = await params;
 
-  const { data: tierList, error } = await supabase.from("tier_lists").select("title").eq("id", params.id).single();
+  const { data: tierList, error } = await supabase
+    .from("tier_lists")
+    .select("title")
+    .eq("id", id)
+    .single();
 
   if (error) {
     console.error("Error fetching tier list:", error);
@@ -15,5 +24,5 @@ export default async function TierListPage({ params }: { params: { id: string } 
     return <div>Tier list not found</div>;
   }
 
-  return <TierListLayout id={params.id} tierListName={tierList.title} />;
+  return <TierListLayout id={id} tierListName={tierList.title} />;
 }
